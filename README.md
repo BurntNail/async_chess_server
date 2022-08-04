ee46424b1ffaf282e8dffc72f6a2804a4fbbb2c7
Jack Maguire Server <jackmaguire1234@gmail.com>
2022-08-04 13:19:26 +0000

    change to use a .env and change db password

---
+++ README.md
@@ -0,0 +1,3 @@
+# Async Chess Server
+
+This is a server for an asynchronous game of Chess. The client is written in Rust, but I wanted to write a server in Go to get more Go experience. If you're running this yourself, make sure to add a `.env` file with a variable for `DB_PASSWORD` and to change the server ip in `main.go`
--- main.go
+++ main.go
@@ -3,32 +3,44 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "localdb"
-	dbname   = "async_chess"
+	host   = "localhost"
+	port   = 5432
+	user   = "postgres"
+	dbname = "async_chess"
 )
 
 var GlobalDb *sql.DB
 var DbMutex sync.Mutex
 
 func main() {
+	loadEnv()
+	loadDB()
+	startServer()
+}
+
+func loadEnv() {
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+}
+func loadDB() {
 	DbMutex.Lock()
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, os.Getenv("DB_PASSWORD"), dbname)
 
 	db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	if err := db.Ping(); err != nil {
 		panic(err)
@@ -41,12 +53,17 @@ func main() {
 
 	GlobalDb = db
 	DbMutex.Unlock()
+}
+func startServer() {
+	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
+
 	router.GET("/games/:id", APIGetPieces)
 	router.GET("/newgame/:id", APINewGame)
 	router.GET("/deletegame/:id", APIDeleteGame)
 	router.GET("/deleteall", APIDeleteTable)
 
 	router.Run("109.74.205.63:12345")
+
 }
